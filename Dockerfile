e8330fb0e3d259b91079f9c810508a5b006388c0
nazimgueye <nazimgueye17@gmail.com>
2024-01-17 19:25:26 +0000

    first

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,7 @@
 #ENTRYPOINT ["java", "-jar", "/app.jar"]
 FROM openjdk:21
 VOLUME /tmp
-WORKDIR /app
 EXPOSE 8083
-RUN mvn clean install
 ARG JAR_FILE=target/test-1.jar
 ADD ${JAR_FILE} app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -3,9 +3,17 @@ package com.example.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 
 public class Test {
+
+    @GetMapping("/test")
+        public Date date(){
+        return new Date();
+    }
+
     @GetMapping("/test")
     public String test(){
         return "Hello World!";
